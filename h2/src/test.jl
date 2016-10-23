41999b3fc99f0c0afabf38a248bee76328b2cc7b
朝南 <chn13@hotmail.com>
2016-10-24 00:03:40 +0800

    Finish Bayes in homework 2

--- h2/src/test.jl
+++ h2/src/test.jl
@@ -6,5 +6,5 @@ df_test = readtable("dataset4.txt", separator = '\t')
 # Bayes: ? BayesNet http://nbviewer.jupyter.org/github/sisl/BayesNets.jl/blob/master/doc/BayesNets.ipynb
 #           http://distributionsjl.readthedocs.io/en/latest/
 # Fisher: https://github.com/remusao/LDA.jl
-# SVM: https://github.com/JuliaStats/SVM.jl
+# SVM: https://github.com/JuliaStats/SVM.jl, https://github.com/simonster/LIBSVM.jl
 # MLP: http://serhanaya.github.io/neural-networks-julia-implementation/
