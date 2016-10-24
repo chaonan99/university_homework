764ecba9d857659fb1a8671208cb72e32fc1c568
朝南 <chn13@hotmail.com>
2016-10-24 11:06:51 +0800

    Update h2 report and code

--- h2/src/test.jl
+++ h2/src/test.jl
@@ -5,6 +5,6 @@ df_test = readtable("dataset4.txt", separator = '\t')
 
 # Bayes: ? BayesNet http://nbviewer.jupyter.org/github/sisl/BayesNets.jl/blob/master/doc/BayesNets.ipynb
 #           http://distributionsjl.readthedocs.io/en/latest/
-# Fisher: https://github.com/remusao/LDA.jl
+# Fisher: http://multivariatestatsjl.readthedocs.io/en/latest/lda.html
 # SVM: https://github.com/JuliaStats/SVM.jl, https://github.com/simonster/LIBSVM.jl
 # MLP: http://serhanaya.github.io/neural-networks-julia-implementation/
